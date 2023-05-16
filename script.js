e0a479816e25593786f9ddc79911fb4ae49a434d
Wesley McGinn <wesleymcginn1@gmail.com>
2023-05-15 23:24:31 -0400

    Create simple, responsive menu bar system

---
+++ script.js
@@ -0,0 +1,34 @@
+function ham() {
+    if (document.getElementsByClassName("ham")[0].style.display == "none") {
+        for (i=0; i<document.getElementsByClassName("ham").length; i++) {
+            document.getElementsByClassName("ham")[i].style.display = "block";
+        }
+    } else {
+        for (i=0; i<document.getElementsByClassName("ham").length; i++) {
+            document.getElementsByClassName("ham")[i].style.display = "none";
+        }
+    }
+}
+
+function resize() {
+    if (window.innerWidth <= 800) {
+        for (i=0; i<document.getElementsByClassName("ham").length; i++) {
+            document.getElementsByClassName("ham")[i].style.display = "none";
+        }
+    } else {
+        for (i=0; i<document.getElementsByClassName("ham").length; i++) {
+            document.getElementsByClassName("ham")[i].style.display = "block";
+        }
+    }
+}
+
+function unselectHam() {
+    if (document.getElementsByClassName("ham")[0].style.display != "none") {
+        if (window.innerWidth <= 800) {
+            for (i=0; i<document.getElementsByClassName("ham").length; i++) {
+                document.getElementsByClassName("ham")[i].style.display = "none";
+            }
+        }
+    }
+}
+
