95784e84c755542bd8a83d901c3e738b95bb5085
Wesley <wesleymcginn1@gmail.com>
2023-05-19 14:31:49 -0400

    Create nbpsp_server.js

---
+++ nbpsp_server.js
@@ -0,0 +1,30 @@
+var mainPath = "C:/Users/admin/Desktop/Github Stuff/New-Buffalo-Physics-Skate-Park-Website";
+const fs = require('fs');
+const http = require('http');
+
+http.createServer(function(req, res) {
+    var requestedFile = req.url;
+    if (requestedFile.slice(requestedFile.length - 5, requestedFile.length).indexOf(".") == -1) {
+        requestedFile += ".html";
+    }
+    fs.readFile(mainPath + requestedFile, function (err, data) {
+        if (err) {
+            fs.readFile(mainPath + "home.html", function (err2, data2) {
+                if (err2) {
+                    console.log("ERROR 404: Your home page does not exist!");
+                    console.log("  >> Expected location: " + mainPath + "home.html");
+                    res.writeHead(404);
+                    res.write("<html lang='en'><h1>404</h1><h2>Sorry.  Our server is currently experiencing issues.  This should be fixed soon.  Please wait patiently and try coming back later.  Thank you.</h2><h1 style='font-size:200px'>):</h1></html>");
+                } else {
+                    res.writeHead(200);
+                    res.write(data2);
+                }
+            });
+            res.end();
+        } else {
+            res.writeHead(200);
+            res.write(data);
+            res.end();
+        }
+    });
+}).listen(80);
