b01b9f761caef1ca6850ab9fafedb667bbf7636a
WesleyMcGinn <wesleymcginn1@gmail.com>
2023-05-21 16:32:09 -0400

    Finish nbpsp server file
    The server is now fully operational and ready for real-time usage.

--- nbpsp_server.js
+++ nbpsp_server.js
@@ -1,34 +1,36 @@
 var mainPath = "/home/pi/Desktop/New-Buffalo-Physics-Skate-Park-Website";
+var homePath = "/home.html";
 const fs = require('fs');
 const http = require('http');
 
-http.createServer(function(req, res) {
+function handleRequest(req, res) {
     var requestedFile = req.url;
-    if (req.url.length < 5 || req.url == "/" || req.url.toLowerCase() == "/home") {
-        requestedFile = "/home.html";
+    if (req.url == "/" || req.url.toLowerCase() == "/home") {
+        requestedFile = homePath;
     }
     if (requestedFile.slice(requestedFile.length - 6, requestedFile.length).indexOf(".") == -1) {
         requestedFile += ".html";
     }
     fs.readFile(mainPath + requestedFile, function (err, data) {
         if (err) {
-            fs.readFile(mainPath + "/home.html", function (err2, data2) {
-                if (err2) {
+            fs.readFile(mainPath + homePath, function (err404, data404) {
+                if (err404) {
+                    console.log();
                     console.log("ERROR 404: Your home page does not exist!");
-                    console.log("  >> Expected location: " + mainPath + "/home.html");
+                    console.log("  >> Expected location: " + mainPath + homePath);
                     res.writeHead(404, {'Content-Type':'text/html'});
-                    res.write("<html lang='en'><h1>404</h1><h2>Sorry.  Our server is currently experiencing issues.  This should be fixed soon.  Please wait patiently and try coming back later.  Thank you.</h2><h1 style='font-size:200px'>):</h1></html>");
+                    res.write("<html lang='en'><h1>404</h1><h2>Sorry.  You tried to visit a page on our site that doesn't exist, and our server is currently experiencing issues.  Please wait patiently and try coming back later.  Thank you.</h2><h1 style='font-size:200px'>):</h1></html>");
+                    res.end();
                 } else {
-                    res.writeHead(200);
-                    res.write(data2);
-                    console.log("No File:");
-                    console.log("  >> Someone tried to access a file on the server that does not exist.");
-                    console.log("  >> Path: " + requestedFile);
+                    console.log();
+                    console.log(" ~ 404 ~ Someone tried to access a nonexistent file: " + req.url);
+                    res.writeHead(404);
+                    res.write(data404);
+                    res.end();
                 }
             });
-            res.end();
         } else {
-            if (requestedFile.indexOf(".svg") != -1) {
+            if (requestedFile.length - requestedFile.lastIndexOf(".svg") == 4) {
                 res.writeHead(200, {'Content-Type':'image/svg+xml'});
             } else {
                 res.writeHead(200);
@@ -37,4 +39,6 @@ http.createServer(function(req, res) {
             res.end();
         }
     });
-}).listen(80);
+}
+
+http.createServer(handleRequest).listen(80);
