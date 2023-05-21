e53f9f96cf5eab8fac535ad1097cfe9986ea09bd
Wesley McGinn <wesleymcginn1@gmail.com>
2023-05-20 20:45:55 -0400

    Create experimental sensor page

---
+++ scripts/sensors.js
@@ -0,0 +1,28 @@
+function setVal(ElementID, Val) {
+    document.getElementById(ElementID).innerHTML = Val.toString();
+}
+
+window.addEventListener("deviceorientation", function(event) {
+    setVal("gx", event.alpha);
+    setVal("gy", event.beta);
+    setVal("gz", event.gamma);
+    setVal("accel", JSON.stringify(event.acceleration));
+    setVal("ax", event.acceleration.x);
+    setVal("ay", event.acceleration.y);
+    setVal("az", event.acceleration.z);
+}, true);
+
+var vibe = {
+    len : function(Duration) {
+        navigator.vibrate(Duration);
+    },
+    blink : function() {
+        window.setInterval(function() {
+            navigator.vibrate(500);
+        }, 800);
+    }
+}
+
+window.setInterval(function() {
+    navigator.getBattery().then((battery)=>{setVal("battery", Math.round(battery.level*100).toString()+"%")});
+}, 1000);
