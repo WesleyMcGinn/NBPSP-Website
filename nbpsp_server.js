09db1b5121edce0c557f1f1d284608e03f16e069
WesleyMcGinn <wesleymcginn1@gmail.com>
2023-05-21 00:04:11 -0400

    Update nbpsp_server.js

--- nbpsp_server.js
+++ nbpsp_server.js
@@ -4,25 +4,35 @@ const http = require('http');
 
 http.createServer(function(req, res) {
     var requestedFile = req.url;
-    if (requestedFile.slice(requestedFile.length - 5, requestedFile.length).indexOf(".") == -1) {
+    if (req.url.length < 5 || req.url == "/" || req.url.toLowerCase() == "/home") {
+        requestedFile = "/home.html";
+    }
+    if (requestedFile.slice(requestedFile.length - 6, requestedFile.length).indexOf(".") == -1) {
         requestedFile += ".html";
     }
     fs.readFile(mainPath + requestedFile, function (err, data) {
         if (err) {
-            fs.readFile(mainPath + "home.html", function (err2, data2) {
+            fs.readFile(mainPath + "/home.html", function (err2, data2) {
                 if (err2) {
                     console.log("ERROR 404: Your home page does not exist!");
-                    console.log("  >> Expected location: " + mainPath + "home.html");
-                    res.writeHead(404);
+                    console.log("  >> Expected location: " + mainPath + "/home.html");
+                    res.writeHead(404, {'Content-Type':'text/html'});
                     res.write("<html lang='en'><h1>404</h1><h2>Sorry.  Our server is currently experiencing issues.  This should be fixed soon.  Please wait patiently and try coming back later.  Thank you.</h2><h1 style='font-size:200px'>):</h1></html>");
                 } else {
                     res.writeHead(200);
                     res.write(data2);
+                    console.log("No File:");
+                    console.log("  >> Someone tried to access a file on the server that does not exist.");
+                    console.log("  >> Path: " + requestedFile);
                 }
             });
             res.end();
         } else {
-            res.writeHead(200);
+            if (requestedFile.indexOf(".svg") != -1) {
+                res.writeHead(200, {'Content-Type':'image/svg+xml'});
+            } else {
+                res.writeHead(200);
+            }
             res.write(data);
             res.end();
         }
